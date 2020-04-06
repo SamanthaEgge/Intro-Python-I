ffa94415f4187ba92cb7ca11a4669ac3bc3fc1bc
Samantha Egge <samantha.c.egge@gmail.com>
2020-04-06 16:52:57 -0700

    Completed python challenges 1, 2, 3, 5.

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -14,8 +14,12 @@ y = "7"
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
+z = int(y)
+print(x+z)
 
 
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+b = x+z+45
+print(str(b))
