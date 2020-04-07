1eaeb4501ffad6b0f80cd4ef1fbc2efed950b265
Samantha Egge <samantha.c.egge@gmail.com>
2020-04-07 13:35:58 -0700

    Completed file_io testing.

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -8,8 +8,11 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
 # Note: pay close attention to your current directory when trying to open "foo.txt"
-
 # YOUR CODE HERE
+with open('src/foo.txt') as foo:
+  print(foo.read())
+
+foo.closed
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +20,5 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+bar = open('src/bar.txt', 'w')
+bar.write('Do I know how to \n Use Python? \n Perhaps not')
